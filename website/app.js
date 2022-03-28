888dca7840cb451ba39fa7a479b028a00d9722c6
Michael S <michael.smales@gmail.com>
2022-03-28 12:02:22 -0500

    refactor app.js

--- website/app.js
+++ website/app.js
@@ -1,19 +1,17 @@
 // Personal API Key for OpenWeatherMap API
 const apiKey = "373b3c94ddd7cced9679c7abb6cfcdf2&units=imperial";
-// baseUrl = "https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}"
+const baseUrl = "https://api.openweathermap.org/data/2.5/weather?";
+const geoBaseUrl = "http://api.openweathermap.org/geo/1.0/zip?";
 
-// chained promises retunring the temperature from openweathermap.org
-// TODO replace zip with user input from page 
-const zip = "60614"
+// TODO replace zip with user input from page once this runs off generate button
+zip = 60614; 
+// const zip = document.getElementById("zip").value;
+// console.log ("zip: ", zip);
 
-getCoords(zip, apiKey) // get the coordinates
-.then(coords => getCoordWeather(coords.lat, coords.lon, apiKey)) // pass lat and long to get weather data
-.then(weatherData => postData('/addEntry', {"date": weatherData.dt, "temp": weatherData.main.temp, "content": "abc"})); // pass weather data to server to log
-
-
-async function getCoords(zip, apiKey) { 
+// functions
+async function getCoords(zip, apiKey, geoBaseUrl) { 
     // function to convert zip into lat an long using openweathermap.org
-    const res = await fetch(`http://api.openweathermap.org/geo/1.0/zip?zip=${zip},US&appid=${apiKey}`);
+    const res = await fetch(`${geoBaseUrl}zip=${zip},US&appid=${apiKey}`);
     try {
         const data = await res.json();
         console.log(zip, data.lat, data.lon);
@@ -24,10 +22,11 @@ async function getCoords(zip, apiKey) {
     }
 }
 
-async function getCoordWeather(lat, lon, apiKey) {
+
+async function getCoordWeather(lat, lon, apiKey, baseUrl) {
     // function to get the current weather based on latitude and longitude
     console.log("running 2nd function", lat, lon)
-    const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`);
+    const res = await fetch(`${baseUrl}lat=${lat}&lon=${lon}&appid=${apiKey}`);
     try {
         const data = await res.json();
         console.log(zip, data);
@@ -38,23 +37,41 @@ async function getCoordWeather(lat, lon, apiKey) {
     }
 }
 
-// post request to send client-side data to server
+
+function createEntry  (date, temp, userContent) {
+    const newEntry = {
+        "date": date, 
+        "temp": temp, 
+        "content": userContent
+    }
+    return(newEntry);
+}
+
+
 async function postData ( url = '', data = {}) {
+    // post request to send client-side data to server
     console.log(data);
     const response = await fetch(url, {
         method: 'POST', 
         credentials: 'same-origin',
         headers: {
             'Content-Type': 'application/json',
-        },
+        },    
         // Body data type must match "Content-Type" header        
         body: JSON.stringify(data), 
-    });
+    });    
     
     try {
         const responseData = await response.json();
         console.log("post response: ", responseData);
     } catch(error) {
         console.log("error: ", error);
-    }
-}
+    }    
+}    
+
+// chained promises
+getCoords(zip, apiKey, geoBaseUrl) // 1. get the coordinates corresponding to zip
+.then(coords => getCoordWeather(coords.lat, coords.lon, apiKey, baseUrl)) // 2. pass lat and long to get weather data
+.then(weatherData => createEntry(weatherData.dt, weatherData.main.temp, "dummy_content")) // 3. collate the new entry
+.then(newEntry => postData('/addEntry', newEntry)); // 4. pass weather data & user content to server to log
+
